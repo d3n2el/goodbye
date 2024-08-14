7598a28dee3d28a2c79de0ae86fcbd31159b8b03
d3n2el <118548099+d3n2el@users.noreply.github.com>
2024-08-14 20:32:43 +0200

    Add files via upload

--- goodbye.py
+++ goodbye.py
@@ -4,10 +4,9 @@ def main():
     while True:
         try:
             user_input= input("Name:")
-            goodbye.append(user_input)
+            s= goodbye.append(user_input)
         except EOFError:
             break
-    print(f"goodbye goodbye, to {adieu} ")
-
+    print("goodbye, goodbye, to " + ", ".join(goodbye))
 
 main()
